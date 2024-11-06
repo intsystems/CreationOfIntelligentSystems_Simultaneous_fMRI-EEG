d19bbec87c8652eb005859249b19e5bdabbbf000
sem-k32 <kirill.semkin32@mail.ru>
2024-11-06 15:56:08 +0300

    EEG encoder train draft.

---
+++ train/encoder/optimizer.py
@@ -0,0 +1,11 @@
+""" define your optimizer and connected stuff here to be used in training
+"""
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+
+def get_optimizer(model: nn.Module) -> optim.optimizer.Optimizer:
+    LR = 1e-3
+    
+    return optim.Adam(model.parameters(), lr=LR)
---
+++ train/encoder/train.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+from datetime import datetime
+import yaml
+from tqdm import tqdm
+
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+from src.egg_encoder import EEG_Encoder
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
+from src.dataset import build_dataloaders
+from optimizer import get_optimizer
+
+
+# load config
+with open("config.yaml", "r") as f:
+    config: dict = yaml.full_load(f)
+
+# create current launch dir
+launch_dir  = Path("launch/" + str(datetime.now()))
+launch_dir.mkdir(parents=True)
+
+# make Tensorboard writer
+writer_log_dir = launch_dir / "runs"
+writer_log_dir.mkdir()
+writer = SummaryWriter(writer_log_dir)
+
+# define device
+device = torch.device(config["device"])
+
+# build dataloaders
+train_dataloader, test_dataloader = build_dataloaders(config["train_data_ratio"], config["batch_size"])
+
+# create encoder
+eeg_encoder = EEG_Encoder(**config["eeg_encoder"]).to(device)
+
+# load CLIP model
+clip_vision = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32").to(device)
+
+# define optimizer
+optimizer = get_optimizer(eeg_encoder.parameters())
+
+# counter for overall step in training process
+global_step = 0
+
+for epoch in tqdm(range(config["num_epochs"]), desc="Epoch"):
+    eeg_encoder.train()
+
+    for batch in train_dataloader:
+        optimizer.zero_grad()
+
+        eeg_signal = batch["eeg"].to(device)
+        imgs = batch["frames"].to(device)
+
+        # get mean CLIP embedding out of given frames
+        num_frames = imgs.shape[1]
+        clip_emb = sum(
+            [clip_vision(imgs[:, frame_num, ...]) / num_frames for frame_num in range(num_frames)]
+        )
+
+        model_emb = eeg_encoder(eeg_signal)
+        loss = F.mse_loss(model_emb, clip_emb)
+
+        loss.backward()
+        optimizer.step()
+
+        writer.add_scalar("Train/MSE", loss.item(), global_step)
+        global_step += 1
+
+    # model's evalutation
+    eeg_encoder.eval()
+    with torch.no_grad():
+        for batch in test_dataloader:
+            eeg_signal = batch["eeg"].to(device)
+            imgs = batch["frames"].to(device)
+
+            # get mean CLIP embedding out of given frames
+            clip_emb = sum(
+                [clip_vision(imgs[:, frame_num, ...]) / imgs.shape[1] for frame_num in range(imgs.shape[1])]
+            )
+
+            model_emb = eeg_encoder(eeg_signal)
+            loss = F.mse_loss(model_emb, clip_emb)
+
+            writer.add_scalar("Test/MSE", loss.item(), global_step)
+            global_step += 1
+
+    # save model
+    torch.save(eeg_encoder.state_dict(), launch_dir / "model.pt")
+
+writer.close()

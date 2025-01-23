cfe616688d10731c88ea0282514836bd702a1f52
sem-k32 <kirill.semkin32@mail.ru>
2025-01-23 19:53:17 +0300

    Our EEG Encoder training for THINGS_EEG dataset. Made to test encoder's correctness.

---
+++ train/eeg_correctness/data_utils.py
@@ -0,0 +1,136 @@
+from pathlib import Path
+import re
+
+import numpy as np
+import torch
+
+from torch.utils.data import Dataset, Sampler, DataLoader, DistributedSampler
+
+
+class EegImgLatentDataset(Dataset):
+    def __init__(
+        self,
+        img_latent_path: Path,
+        eeg_paths: dict[int, Path],    # map of paths participant_id -> participant file
+    ):
+        """ each eeg has dim=(img_num, trial_num, channel, time)
+        """
+        super().__init__()
+
+        # img latents can be loaded fully
+        self._img_latents: torch.Tensor = torch.load(img_latent_path)
+
+        # load eeg's using mmap (not fully in RAM)
+        self._eegs = [np.load(eeg_path, mmap_mode='r') for eeg_path in eeg_paths.values()]
+        # save participants' ids
+        self._participants = list(eeg_paths.keys())
+
+        # it is assumed that each participant has equal number of eegs
+        # it equals num_imgs * num_trials
+        self._eeg_set_shape = self._eegs[0].shape
+        self._eegs_per_partic = self._eeg_set_shape[0] * self._eeg_set_shape[1]
+
+        # compute total number of eegs
+        self._total_eegs = sum(list(eeg.shape[0] * eeg.shape[1] for eeg in self._eegs))
+
+    def __len__(self):
+        return self._total_eegs
+
+    def __getitem__(self, indx):
+        """ returns tuple(participant_id, eeg, img_latent)
+        """
+        partic_num = indx // self._eegs_per_partic
+        eeg_num = indx % self._eegs_per_partic
+        img_num = eeg_num // self._eeg_set_shape[0]
+        trial_num = eeg_num % self._eeg_set_shape[0]
+
+        return (
+            self._participants[partic_num],
+            torch.from_numpy(self._eegs[partic_num][img_num, trial_num]).float(),
+            self._img_latents[img_num]
+        )
+
+
+class ClipSampler(Sampler):
+    """ Orders eeg examples so it is basically strips of unique images.
+        Is used to get diverse batches for CLIP-loss training.
+    """
+    def __init__(self, dataset: EegImgLatentDataset):
+        """_summary_
+
+        Args:
+            dataset (EegImgLatentDataset): is used as a friend class
+        """
+        self._num_eegs = len(dataset._eegs)
+        self._num_trials = dataset._eeg_set_shape[1]
+        self._num_imgs = dataset._eeg_set_shape[0]
+
+    def _get_random_grouped_indxs(self):
+        rand_gen = np.random.default_rng()
+
+        all_indxs = np.arange(0, self._num_eegs * self._num_trials * self._num_imgs)\
+                      .reshape(self._num_eegs, self._num_imgs, self._num_trials)\
+                      .swapaxes(0, 1)\
+                      .reshape(-1, self._num_imgs)
+        # permute img examples for all images
+        all_indxs = rand_gen.permuted(all_indxs, axis=0, out=all_indxs)
+        # permute order of the img examples in each row
+        all_indxs = rand_gen.permuted(all_indxs, axis=1, out=all_indxs)
+
+        return all_indxs
+
+    def __iter__(self):
+        all_indxs = self._get_random_grouped_indxs()
+
+        return all_indxs.flat
+
+    def __len__(self):
+        return self._num_eegs * self._num_trials * self._num_imgs
+
+
+class DistributedClipSampler(DistributedSampler, ClipSampler):
+    """ distributed version of the ClipSampler
+    """
+    def __init__(self, dataset, num_replicas = None, rank = None, shuffle = True, seed = 0, drop_last = False):
+        super(DistributedSampler).__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
+        super(ClipSampler).__init__(dataset)
+
+    def __len__(self):
+        return super(ClipSampler).__len__()
+
+    def __iter__(self):
+        all_indxs = self._get_random_grouped_indxs()
+
+        chunk_start = (len(self) // self.num_replicas) * self.num_replicas
+        chunk_end = min(
+            (len(self) // self.num_replicas) * (self.num_replicas + 1),
+            len(self)
+        )
+
+        # flatten indicies array and return corresponding chunk
+        return np.ravel(all_indxs)[chunk_start:chunk_end].flat
+
+
+def eeg_img_latent_collate(batch_list: list) -> torch.Tensor:
+    participants = torch.LongTensor([exmpl[0] for exmpl in batch_list])
+    eegs = torch.stack([exmpl[1] for exmpl in batch_list])
+    img_latents = torch.stack([exmpl[2] for exmpl in batch_list])
+
+    return participants, eegs, img_latents
+
+
+def parse_subs_eeg_dir(subs_dir: Path):
+    subs = {}
+
+    for sub_dir in subs_dir.glob("subs-*"):
+        sub_num = int(
+            re.match("subs-(?P<sub_num>\d*)", sub_dir.stem).groupdict()["sub_num"]
+        )
+        train_file = next(sub_dir.glob("*train*"))
+        test_file = next(sub_dir.glob("*test*"))
+        
+        subs[sub_num] = {"train": train_file, "test": test_file}
+
+    return subs
+
+
---
+++ train/eeg_correctness/train_eeg_only_contrastive.py
@@ -0,0 +1,168 @@
+from pathlib import Path
+import argparse
+from omegaconf import OmegaConf
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from eeg_encoder import EEGEncoder
+import data_utils
+
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+
+import wandb
+
+
+def contrastive_loss(image_features, brain_features, logit_scale) -> dict:
+    # Normalize features
+    image_features = image_features / image_features.norm(dim=1, keepdim=True)
+    brain_features = brain_features / brain_features.norm(dim=1, keepdim=True)
+
+    # Compute cosine similarity as logits
+    logit_scale = logit_scale.exp()
+    logits_per_image = logit_scale * image_features @ brain_features.T
+    logits_per_brain = logits_per_image.T
+
+    # Compute cross-entropy loss
+    batch_size = logits_per_image.shape[0]
+    labels = torch.arange(batch_size, device=image_features.device, dtype=torch.long)
+
+    total_loss = (F.cross_entropy(logits_per_image, labels) + F.cross_entropy(logits_per_brain, labels)) / 2
+
+    return total_loss, logits_per_brain
+
+
+class LitEggEncoder(L.LightningModule):
+    def __init__(self, eeg_encoder: EEGEncoder, logit_scale: nn.Parameter, config: OmegaConf):
+        super().__init__()
+
+        self.eeg_encoder = eeg_encoder
+        self.optim_config = config.optimizer_kwargs
+        self.logit_scale = logit_scale
+
+        self.save_hyperparameters(config)
+
+    def training_step(self, batch: tuple[torch.Tensor], batch_idx: int):
+        subj_ids, eegs, img_latents = batch
+        eeg_latents = self.eeg_encoder(eegs, subj_ids)
+
+        loss, logits_per_brain = contrastive_loss(img_latents, eeg_latents, self.logit_scale)
+
+        self.log("Train/CLIP_loss", loss.item(), prog_bar=True, on_step=True)
+
+        return loss
+    
+    def validation_step(self, batch: tuple[torch.Tensor], batch_idx: int):
+        subj_ids, eegs, img_latents = batch
+        eeg_latents = self.eeg_encoder(eegs, subj_ids)
+
+        loss, logits_per_brain = contrastive_loss(img_latents, eeg_latents, self.logit_scale)
+
+        self.log("Test/CLIP_loss", loss.item(), on_epoch=True)
+
+        # calculate rest metrics
+        self.log(
+            "Test/accuracy",
+            logits_per_brain.argmax(dim=1) == torch.arange(logits_per_brain.shape[0]),
+            on_epoch=True,
+            reduce_fx=lambda x: torch.mean(torch.concat(x))
+        )
+
+        prob_per_brain: torch.Tensor = torch.softmax(logits_per_brain)
+        self.log(
+            "Test/batch_p_true_mean",
+            torch.gather(prob_per_brain, dim=0, index=torch.arange(logits_per_brain.shape[0])),
+            on_epoch=True,
+            reduce_fx=lambda x: torch.mean(torch.concat(x))
+        )
+        self.log(
+            "Test/batch_p_true_std",
+            torch.gather(prob_per_brain, dim=0, index=torch.arange(logits_per_brain.shape[0])),
+            on_epoch=True,
+            reduce_fx=lambda x: torch.std(torch.concat(x))
+        )
+
+        self.log(
+            "Test/batch_entropy_mean",
+            (prob_per_brain * prob_per_brain.log()).sum(dim=1),
+            on_epoch=True,
+            reduce_fx=lambda x: torch.mean(torch.concat(x))
+        )
+        self.log(
+            "Test/batch_entropy_std",
+            (prob_per_brain * prob_per_brain.log()).sum(dim=1),
+            on_epoch=True,
+            reduce_fx=lambda x: torch.std(torch.concat(x))
+        )
+
+    def configure_optimizers(self):
+        return optim.Adam(self.eeg_encoder.parameters(), **self.optim_config)
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_path", type=str, default="eeg_sole.yaml",
+                        required=False, help="Path to the configuration file.")
+    args = parser.parse_args()
+
+    # load config file
+    config = OmegaConf.load(args["config_path"])
+
+    logger = WandbLogger(
+        project="EEG Debug",
+        log_model=True
+    )
+
+    # load data
+    subs_paths = data_utils.parse_subs_eeg_dir(Path(config.subs_dir_path))
+    subs_train_paths = {sub_num: path_dict["train"] for sub_num, path_dict in subs_paths.items()}
+    subs_test_paths = {sub_num: path_dict["test"] for sub_num, path_dict in subs_paths.items()}
+
+    train_dataset = data_utils.EegImgLatentDataset(
+        Path(config.img_latent_train_path),
+        subs_train_paths
+    )
+    test_dataset = data_utils.EegImgLatentDataset(
+        Path(config.img_latent_test_path),
+        subs_test_paths
+    )
+
+    # configure special samplers to assure batch diversity for CLIP-loss
+    if config.num_gpus == 1:
+        train_sampler = data_utils.ClipSampler(train_dataset)
+        test_sampler = data_utils.ClipSampler(test_dataset)
+    else:
+        train_sampler = data_utils.DistributedClipSampler(train_dataset)
+        test_sampler = data_utils.DistributedClipSampler(test_dataset)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config.batch_size,
+        sampler=train_sampler
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=config.batch_size,
+        sampler=test_sampler
+    )
+
+    # create lightning model
+    model = LitEggEncoder(
+        EEGEncoder(**config.eeg_kwargs),
+        nn.Parameter(torch.FloatTensor([config.init_logit_scale])),
+        config.optimizer_kwargs
+    )
+    trainer = L.Trainer(
+        logger=logger,
+        use_distributed_sampler=False,
+        accelerator=config.accelerator,
+        devices=config.num_devices,
+        max_epochs=config.max_epochs
+    )
+
+    trainer.fit(model, train_loader, test_loader)
